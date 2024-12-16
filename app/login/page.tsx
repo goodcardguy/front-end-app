bdf528965d58f8edbbd415656bdcc96cf5f69358
goodcardguy <thegoodcardguy@gmail.com>
2024-12-16 09:24:56 -0500

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,55 +28,60 @@ export default function LoginPage() {
         <CardFooter>
           <form
             action={async (e) => {
-              'use server'
-              await signIn('credentials', {
-                userName:e.get('userName'),
-                password:e.get('password'),
-                redirectTo: '/', // Replace with your desired callback URL
-              });
+              'use server';
+              try {
+                await signIn('credentials', {
+                  userName: e.get('userName'),
+                  password: e.get('password'),
+                  redirectTo: '/' // Replace with your desired callback URL
+                });
+              } catch {
+                console.log('error');
+                alert("Looks like those creditials don't work! Please make sure you are using the correct ones.")
+              }
             }}
             className="w-full"
           >
-          <div className="mb-4">
-            <label
-              htmlFor="userName"
-              className="block text-sm font-medium text-gray-700"
+            <div className="mb-4">
+              <label
+                htmlFor="userName"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id="userName"
+                name="userName"
+                // value={userName}
+                // onChange={(e) => setUserName(e.target.value)}
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                // value={password}
+                // onChange={(e) => setPassword(e.target.value)}
+                required
+                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+              />
+            </div>
+            <Button
+              // onClick={() => submitCreds(userName, password)}
+              className="w-full"
             >
-              Username
-            </label>
-            <input
-              type="text"
-              id="userName"
-              name="userName"
-              // value={userName}
-              // onChange={(e) => setUserName(e.target.value)}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              // value={password}
-              // onChange={(e) => setPassword(e.target.value)}
-              required
-              className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-            />
-          </div>
-          <Button
-            // onClick={() => submitCreds(userName, password)}
-            className="w-full"
-          >
-            Sign in
-          </Button>
+              Sign in
+            </Button>
           </form>
         </CardFooter>
       </Card>
